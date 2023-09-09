21890cf104ae5d9c29a3e2edab938a345f00cf96
André Valim <andre.valim@edu.univali.br>
2023-09-09 15:26:52 -0300

    media queries implementadas para apresentação, sobre, portfolio e experiência

--- navbar.js
+++ navbar.js
@@ -57,7 +57,7 @@ let esconderNavbar = function esconderNavbar() {
         item.style.visibility = "hidden";
     });
     
-    document.getElementById("cabecalho-navbar").style.width = "4.3vh";
+    document.getElementById("cabecalho-navbar").style.width = "auto";
 
     setTimeout(esconderItens, 400, itensNavbar);
 }
